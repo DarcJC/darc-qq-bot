877d9f416ad66d27aa5dd06116de324f7c3eddcf
DarcJC <me@darc.pro>
2021-02-14 17:25:35 +0800

    完成异常处理

--- bot/destiny/index.js
+++ bot/destiny/index.js
@@ -10,11 +10,6 @@ const {commander, bot} = require("./commands")
 module.exports = (ctx) => {
   const mirai = ctx.mirai;
 
-  mirai.api.handleStatusCode().then((value)=>{
-  }, (reason) => {
-    mirai.api.sendGroupMessage(`发生错误:\n${reason}`, 866368474)
-  })
-
   mirai.on("GroupRecallEvent", ({ operator, messageId, authorId }) => {
     if (operator.permission != 'OWNER') return
     const text = `${operator.memberName} 撤回了一条消息, 并拜托${authorId}不要再发色图了。`;
--- bot/index.js
+++ bot/index.js
@@ -2,7 +2,18 @@ const Bot = require("el-bot");
 const el = require("../el");
 
 const bot = new Bot(el);
+bot.mirai.api.axios.interceptors.response.use(
+    res => {
+        return res
+    },
+    (reason) => {
+        bot.mirai.api.sendGroupMessage(`发生错误:\n${reason}`, 866368474)
+        return new Promise(()=>{})
+    }
+)
+
 bot.start()
+
 // bot.start((msg) => {
 //   if (bot.isDev) console.log(msg);
 // });
